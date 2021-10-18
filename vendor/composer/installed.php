5133d1d8ab687019a779ab5c34d30ff8254e7b5c
Jessie Durand <90646375+jessie-durand@users.noreply.github.com>
2021-10-18 16:30:38 +0200

    Add files via upload

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '9a66bfc1fc232ca7fc972dffc2f0fb5125e22258',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '9a66bfc1fc232ca7fc972dffc2f0fb5125e22258',
+            'dev_requirement' => false,
+        ),
+        'gipetto/cowsay' => array(
+            'pretty_version' => '1.1.0',
+            'version' => '1.1.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../gipetto/cowsay',
+            'aliases' => array(),
+            'reference' => '85218ee4c0d18cc8753e08af54a8740aac12860a',
+            'dev_requirement' => false,
+        ),
+    ),
+);
